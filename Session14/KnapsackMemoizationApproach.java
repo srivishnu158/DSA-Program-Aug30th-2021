ac36f7b2724bdb014cff9b1b979dd4324cf0421d
Srinu Nampalli <srinunampalli@Srinus-MacBook-Pro.local>
2021-09-21 08:58:35 +0530

    01 Knapsack Memoization , Soon Notes will be updated

--- Session14/KnapsackMemoizationApproach.java
+++ Session14/KnapsackMemoizationApproach.java
@@ -2,19 +2,21 @@ class Sample
 {
 
 	/*
-		Time Complexity : O(n*capacity) dp[n][capacity] => n*capacity
+		Time Complexity : O(n*capacity) , 
+		As we derive max possible subproblems dp[n][capacity] => n*capacity
+
 		Space Complexity : O(n*capacity)
 	*/
+
 	public int knapsackProblem(int[] weights, int[] profits, 
 		int currentIndex , int capacity)
 	{
 
-		// Base check
+		// Base checks
 		if(weights.length != profits.length) 
 		{
 			return -1;
 		}
-
 		int n = weights.length;
 
 		int[][] dp = new int[n][capacity+1];
@@ -29,7 +31,6 @@ class Sample
 
 		return memoizationApproach(weights,profits, currentIndex,capacity,dp);
 	}
-
 
 	public int  memoizationApproach(int[] weights, int[] profits, 
 		int currentIndex , int capacity, int[][] dp)
@@ -72,6 +73,27 @@ public class KnapsackMemoizationApproach
 {
 	public static void main(String args[])
 	{
+			Sample s = new Sample();
+		int[] weights = {1,2,3};
+		int[] profits = {5,2,4};
+		int currentIndex = 0;
+		int capacity = 5;
+
+		int maxProfit = s.knapsackProblem(weights,profits,currentIndex,capacity);
+
+		System.out.println(" MaxProfit => "+maxProfit);
+	}
+}
+
+
+
+
+
+/*
+Logic to test timetaken on higher inputs
+
+public static void main(String args[])
+	{
 		Sample s = new Sample();
 		int[] weights = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22};
 		int[] profits = {1,3,5,7,11,13,15,17,19,21,23,25,27,29,31,33,35,37,39,41,43,45,1,3,5,7,11,13,15,17,19,21,23,25,27,29,31,33,35,37,39,41,43,45};
@@ -89,15 +111,8 @@ public class KnapsackMemoizationApproach
 		System.out.println(" Time Taken => "+(endTime - startTime));
 
 	}
-}
 
-
-
-
-
-
-
-
+*/
 
 
 
